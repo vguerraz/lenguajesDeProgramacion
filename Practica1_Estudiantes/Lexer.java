22f63bc9e66a04b06b2a26566ea46f15b7d0a48e
cpalacior <82948098+cpalacior@users.noreply.github.com>
2021-09-13 18:15:44 -0500

    Update Lexer.java

--- Practica1_Estudiantes/Lexer.java
+++ Practica1_Estudiantes/Lexer.java
@@ -1,15 +1,17 @@
 /** Programming Languajes
-    By: Carmen Carvajal
-    Lexer class: This class generates an arraylist containing every token in the source code
-        
-**/
+By: Carmen Carvajal
+Lexer class: This class generates an arraylist containing every token in the source code
+
+ **/
+
 
 import java.util.*;
 import java.lang.*;
 import java.io.*;
 
+
 public class Lexer{
-    
+
     //Define token codes based on the grammar
     public static final int PROGRAM = 1;
     public static final int ENDPROGRAM = 2;
@@ -29,46 +31,46 @@ public class Lexer{
     public static final int PRINT = 16;
     public static final int CALL = 17;
     public static final int WHILE = 18;
-    public static final int DIFERENTE = 19; 
-    public static final int SUMA = 20;
-    public static final int MULTIPLICACION = 21;
+    public static final int DIFERENTE = 20;
+    public static final int SUMA = 21;
+    public static final int MULTIPLICACION = 22;
     public static final int INVALIDTOKEN=98;
     public static final int EOF = 99;
 
+
     //fileScanner: (Scanner) to iterate over the source file
     private Scanner fileScanner;
-    
+
     //lineScanner: (Scanner) to iterate over each line in the source file
     private Scanner lineScanner;
     //keywordsTable: (ArrayList) stores the language keywords and symbols (e.g., print, read, program, etc...)
-    public ArrayList<Token>             keywordsTable;
+    public ArrayList<Token> keywordsTable;
     //tokenList: (ArrayList) contains every token in the source file
-    private ArrayList<Token>             tokenList;
+    private ArrayList<Token> tokenList;
     //tokenIterator: (ListIterator) used for visiting each token in the tokenList object
-    private ListIterator<Token>            tokenIterator;
+    private ListIterator<Token> tokenIterator;
     //currentToken: (Token) stores the current token where the tokenIterator is pointing to
-    private Token                        currentToken;
+    private Token currentToken;
     //lineCount: (int) count line in the each source file
-    private int                            lineCount;
-    
+    private int lineCount;
 
 
     /**
-        Constructor:
-            Initialize the keywordsTable
-            recognize and store each token in the source file
-            initialize the token iterator
-        
-            @param fileName: source file path
-    **/
+    Constructor:
+    Initialize the keywordsTable
+    recognize and store each token in the source file
+    initialize the token iterator
+
+    @param fileName: source file path
+     **/
     public Lexer (String fileName) throws FileNotFoundException {
-        
+
         //Define an scanner for the source file
         fileScanner = new Scanner(new File(fileName));
-        
+
         //Initialize the linecount
         lineCount=0;
-        
+
         //Initialize the keyword table
         keywordsTable = new ArrayList<Token>();
         keywordsTable.add(new Token(PROGRAM, "program", 0));
@@ -91,8 +93,7 @@ public class Lexer{
         keywordsTable.add(new Token(SUMA, "+", 0));
         keywordsTable.add(new Token(MULTIPLICACION, "*", 0));
         keywordsTable.add(new Token(EOF, "EOF", 0));
-        
-        
+
         /** extract each token from the source file **/
         String tokenText;
         int tokenCode;
@@ -111,13 +112,15 @@ public class Lexer{
         }
         while(tokenText.compareTo("EOF")!=0);
 
+
         //Initialize the token iterator pointing to before the first element in the list
         tokenIterator = tokenList.listIterator();
-        
+
     }
 
+
     /* Function nextText: extract each token from the source file
-    */
+     */
     public String nextText(){
         //actual token text`
         String text;
@@ -146,35 +149,38 @@ public class Lexer{
                 } else {
                     //End of line, discard the lineScanner
                     lineScanner = null;
-                }  
+                }
+
             }
         } while(text ==null);
-        
+
         //Return the token
         return text;
     }
 
+
     /**
-        Function nextToken: point the listIterator to the next token and return it
-    **/
+    Function nextToken: point the listIterator to the next token and return it
+     **/
     public Token nextToken()
-    {        
+    {
         //move the list iterator to the next token
-        currentToken = tokenIterator.next(); 
-        return currentToken;
-    }
-    
-    /**
-        Function getCurrentToken: return the token to which the listIterator is pointing to
-    **/
-    public Token getCurrentToken(){
+        currentToken = tokenIterator.next();
         return currentToken;
     }
 
     /**
-        Function getTokenCode: Search for the token code in the keywordsTable
-            @param tokenText: (String) actual token text found in the source file
-    **/
+    Function getCurrentToken: return the token to which the listIterator is pointing to
+     **/
+    public Token getCurrentToken(){
+        return currentToken;
+    }
+
+
+    /**
+    Function getTokenCode: Search for the token code in the keywordsTable
+    @param tokenText: (String) actual token text found in the source file
+     **/
     public int getTokenCode(String tokenText)
     {
         //Create an iterator to traverse the keywordTable
@@ -188,13 +194,13 @@ public class Lexer{
             tmpToken = it.next();
             //Verify if the actual token text is equals to the iterator point
             if (tmpToken.text.compareTo(tokenText)==0)
-            tokenCode = tmpToken.code;
+                tokenCode = tmpToken.code;
         }
-        
+
         //If the token was not found, it is a variable
         if (tokenCode ==-1)
             tokenCode = VARIABLE;
-        
+
         //Return the actual token code
         return tokenCode;
     }
